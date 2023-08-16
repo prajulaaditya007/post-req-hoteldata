49284679c79c5c8f4a1638d71b4c98cf7c33c4e1
Prajul <prajulaadityaa@gmail.com>
2023-08-16 11:36:58 +0530

    typeahead system

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 
 import HotelGuest from "./HotelGuest.tsx";
 import './App.css'
+import Typeaheads from "./components/Typeaheads.tsx";
 
 function App() {
 
     return (
         <>
+            <Typeaheads/>
             <h1>Hotel Guest App</h1>
             <HotelGuest/>
+
         </>
     )
 }
